403129c88b08d968636d9029c3783526d8f78499
Klesaak <ihor.237@gmail.com>
2023-04-22 01:34:30 +0300

    upd

--- src/main/java/ua/klesaak/mineperms/bukkit/integration/WorldEditPermissionProvider.java
+++ src/main/java/ua/klesaak/mineperms/bukkit/integration/WorldEditPermissionProvider.java
@@ -1,11 +1,53 @@
 package ua.klesaak.mineperms.bukkit.integration;
 
+import com.sk89q.wepif.PermissionsProvider;
+import org.bukkit.OfflinePlayer;
 import ua.klesaak.mineperms.bukkit.MinePermsBukkit;
 
-public class WorldEditPermissionProvider {
+public class WorldEditPermissionProvider implements PermissionsProvider {
     private final MinePermsBukkit manager;
 
     public WorldEditPermissionProvider(MinePermsBukkit manager) {
         this.manager = manager;
+    }
+
+    @Override
+    public boolean hasPermission(String name, String permission) {
+        return false;
+    }
+
+    @Override
+    public boolean hasPermission(String worldName, String name, String permission) {
+        return false;
+    }
+
+    @Override
+    public boolean inGroup(String player, String group) {
+        return false;
+    }
+
+    @Override
+    public String[] getGroups(String player) {
+        return new String[0];
+    }
+
+    @Override
+    public boolean hasPermission(OfflinePlayer player, String permission) {
+        return false;
+    }
+
+    @Override
+    public boolean hasPermission(String worldName, OfflinePlayer player, String permission) {
+        return false;
+    }
+
+    @Override
+    public boolean inGroup(OfflinePlayer player, String group) {
+        return false;
+    }
+
+    @Override
+    public String[] getGroups(OfflinePlayer player) {
+        return new String[0];
     }
 }
