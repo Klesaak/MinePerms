d5f07b1d20ec098deab4de04bf13fe4bed440923
Klesaak <ihor.237@gmail.com>
2023-04-22 01:21:25 +0300

    upd

--- src/main/java/ua/klesaak/mineperms/api/MinePermsAPI.java
+++ src/main/java/ua/klesaak/mineperms/api/MinePermsAPI.java
@@ -1,4 +1,41 @@
 package ua.klesaak.mineperms.api;
 
+import lombok.experimental.UtilityClass;
+
+import java.util.UUID;
+
+@UtilityClass
 public class MinePermsAPI {
+
+    public boolean hasPermission(String playerName, String permission) {
+        return false;
+    }
+
+    public boolean hasPermission(UUID uuid, String permission) {
+        return false;
+    }
+
+    public String getUserGroup(String playerName) {
+        return "";
+    }
+
+    public String getUserGroup(UUID uuid) {
+        return "";
+    }
+
+    public String getUserPrefix(String playerName) {
+        return "";
+    }
+
+    public String getUserPrefix(UUID uuid) {
+        return "";
+    }
+
+    public String getUserSuffix(String playerName) {
+        return "";
+    }
+
+    public String getUserSuffix(UUID uuid) {
+        return "";
+    }
 }
