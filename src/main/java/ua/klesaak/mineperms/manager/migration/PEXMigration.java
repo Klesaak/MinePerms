cbee12311e6f5f15765607a9d4b9c9feb9627889
Klesaak <ihor.237@gmail.com>
2023-06-03 00:53:46 +0300

    upd

--- src/main/java/ua/klesaak/mineperms/manager/migration/PEXMigration.java
+++ src/main/java/ua/klesaak/mineperms/manager/migration/PEXMigration.java
@@ -1,4 +1,13 @@
 package ua.klesaak.mineperms.manager.migration;
 
+import lombok.val;
+import ru.tehkode.permissions.bukkit.PermissionsEx;
+
 public class PEXMigration {
+
+    public void migrate() {//todo
+         val permissionManager = PermissionsEx.getPermissionManager();
+        permissionManager.getUsers();
+        permissionManager.getGroupList();
+    }
 }
--- src/main/java/ua/klesaak/mineperms/manager/migration/SpermMigration.java
+++ src/main/java/ua/klesaak/mineperms/manager/migration/SpermMigration.java
@@ -1,4 +1,13 @@
 package ua.klesaak.mineperms.manager.migration;
 
+import lombok.val;
+import ru.Den_Abr.SimplePerms.SimplePermsCommon;
+
 public class SpermMigration {
+
+    public void migrate() {//todo
+        val backend = SimplePermsCommon.getInstance().getPermissionManager().getBackend();
+        backend.getAllGroups();
+        backend.getAllUsers();
+    }
 }
